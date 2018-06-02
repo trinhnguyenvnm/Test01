ffe2634f6c75a3eb8084f99e12037db6f67255c1
Toan.Phung <toanpk1@fsoft.com.vn>
2018-06-03 00:42:15 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CICD Hello World!';
+  title = 'CICD [Trinh] Hello World!';
 }
